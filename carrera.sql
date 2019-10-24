a3dee429c81aaeaa20c540685806b6cd98498ad4
hebrito <55327383+hebrito@users.noreply.github.com>
2019-10-23 23:52:24 -0400

    Add files via upload

---
+++ carrera.sql
@@ -0,0 +1,2 @@
+INSERT INTO carrera(nombre) VALUES ('Educacion Fisica');
+DElETE from carrera where nombre= 'Educacion Fisica'and id='11';
---
+++ estudiante.sql
@@ -0,0 +1,2 @@
+INSERT INTO estudiante(nombre,matricula, edad)  VALUES ('Avid Duarte', '192109','37');
+DElETE from estudiante where nombre= 'EHelen Reynoso' and matricula= '1920154'and id='8';
